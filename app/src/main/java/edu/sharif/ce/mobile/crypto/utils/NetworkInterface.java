83069fe1f7952ed8a418735da2060dce9fdd8234
aboots <mahdi.abootorabi2@gmail.com>
2021-02-28 00:07:18 +0330

    feat: update crypto image urls

--- app/src/main/java/edu/sharif/ce/mobile/crypto/utils/NetworkInterface.java
+++ app/src/main/java/edu/sharif/ce/mobile/crypto/utils/NetworkInterface.java
@@ -22,6 +22,7 @@ import edu.sharif.ce.mobile.crypto.models.Crypto;
 
 public class NetworkInterface {
     private final static String API_KEY_FOR_COIN_MARKET_CAP = "ae590806-c68e-46c5-8577-e5640c7d4b41";
+    private static ArrayList<Crypto> _cryptoArrayList = new ArrayList<>();
 
     public static void getCryptoData(int start, int limit) {
         OkHttpClient okHttpClient = new OkHttpClient();
@@ -45,7 +46,7 @@ public class NetworkInterface {
             @Override
             public void onResponse(Response response) throws IOException {
                 if (!response.isSuccessful()) {
-                    Log.e("network2", response.body().string());
+                    Log.e("network", response.body().string());
                 } else {
                     String body = response.body().string();
                     Log.d("response", body);
@@ -65,6 +66,9 @@ public class NetworkInterface {
                             crypto.setPercentChange7D(inner_obj.getDouble("percent_change_7d"));
                             cryptoArrayList.add(crypto);
                         }
+                        _cryptoArrayList = cryptoArrayList;
+                        ;
+                        NetworkInterface.getCryptoImageUrls(cryptoArrayList);
                     } catch (JSONException e) {
                         Log.e("json_parser", Objects.requireNonNull(e.getMessage()));
                     }
@@ -72,6 +76,52 @@ public class NetworkInterface {
             }
         });
 
+    }
+
+
+    public static void getCryptoImageUrls(ArrayList<Crypto> cryptoArrayList) {
+        StringBuilder sb = new StringBuilder();
+        for (Crypto crypto : cryptoArrayList) {
+            sb.append(crypto.getId()).append(",");
+        }
+        String query_params = sb.deleteCharAt(sb.length() - 1).toString();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id="
+                .concat(query_params))
+                .newBuilder();
+
+        String url = urlBuilder.build().toString();
+
+        final Request request = new Request.Builder().url(url)
+                .addHeader("X-CMC_PRO_API_KEY", API_KEY_FOR_COIN_MARKET_CAP)
+                .build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("network", Objects.requireNonNull(e.getMessage()));
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Log.e("network", response.body().string());
+                } else {
+                    String body = response.body().string();
+                    Log.d("response", body);
+                    try {
+                        ArrayList<Crypto> cryptoArrayList = _cryptoArrayList;
+                        JSONObject data = new JSONObject(body).getJSONObject("data");
+                        for (Crypto crypto : cryptoArrayList) {
+                            JSONObject object = data.getJSONObject(crypto.getId());
+                            crypto.setImageUrl(object.getString("logo"));
+                        }
+                    } catch (JSONException e) {
+                        Log.e("json_parser", Objects.requireNonNull(e.getMessage()));
+                    }
+                }
+            }
+        });
     }
 }
 
