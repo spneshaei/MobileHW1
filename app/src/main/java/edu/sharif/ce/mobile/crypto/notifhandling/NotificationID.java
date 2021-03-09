3c426490830b2446ef308892b58470ed83ab1f75
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2021-03-09 20:03:38 +0330

    Fixed repeated ID

--- app/src/main/java/edu/sharif/ce/mobile/crypto/notifhandling/NotificationID.java
+++ app/src/main/java/edu/sharif/ce/mobile/crypto/notifhandling/NotificationID.java
@@ -9,6 +9,6 @@ public class NotificationID {
         public static int DATA_LOADED_FROM_CACHE = 1;
         public static int NO_INTERNET_CONNECTION = 2;
         public static int NEW_DATA_LOADED_FOR_RESTER = 3;
-        public static int NEW_DATA_LOADED_FOR_UI = 3;
+        public static int NEW_DATA_LOADED_FOR_UI = 4;
     }
 }
