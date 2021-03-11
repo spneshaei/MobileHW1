e7da996a7d9b250fd4b08a1456a28fed252b4790
aboots <mahdi.abootorabi2@gmail.com>
2021-03-11 12:24:05 +0330

    feat: add splash screen to main activity

--- app/src/main/java/edu/sharif/ce/mobile/crypto/MainActivity.java
+++ app/src/main/java/edu/sharif/ce/mobile/crypto/MainActivity.java
@@ -73,6 +73,8 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
